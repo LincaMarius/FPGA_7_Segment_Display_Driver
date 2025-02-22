c73be0e01ef9987e1c8dd813ca07e33fe44de3a7
LincaMarius <mariusgeorge1173@gmail.com>
2025-02-22 21:57:18 +0200

    new file:   1_ Digit_Display/Aplication_1_tb.v

---
+++ 1_ Digit_Display/Aplication_1_tb.v
@@ -0,0 +1,39 @@
+`timescale 1ns/1ns
+
+// Verilog code for test bench for Aplication 1
+// who uses
+// BCD to one Digit 7-Segment Display FPGA Driver
+
+module Aplication_1_tb;
+
+// input signals of the mode under test
+reg clk50MHz_tb;
+reg rst_n;
+
+// output signals of the module under test
+wire[6:0] SEG_tb;
+reg bp_tb;
+reg SEL7_tb;
+
+initial
+begin
+// initial conditions
+	clk50MHz_tb = 1'b0;
+	rst_n = 1'b0;
+	#100 rst_n = 1'b1;	
+end
+
+// we generate a clock signal with a frequency of 50 MHz
+always#10 clk50MHz_tb = ~clk50MHz_tb;
+
+// we declare the components under test
+BCD_to_1_Digit dut
+(
+	.clk50MHz	(clk50MHz_tb),
+	.rst_n		(rst_n),
+	.Segments 	(SEG_tb),
+	.bp			(bp_tb),
+	.SEL7			(SEL7_tb)
+);
+	
+endmodule 
