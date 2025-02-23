5e3d14846fcddb0103eca570927e1cdbdcfb685c
LincaMarius <mariusgeorge1173@gmail.com>
2025-02-23 05:16:02 +0200

    new file:   1_ Digit_Displa_CC/Aplication_4.v

---
+++ 1_ Digit_Displa_CC/Aplication_4.v
@@ -0,0 +1,39 @@
+
+// Aplicațion for testing
+// Hexadecimal to one Digit 7-Segment Common Cathode Display FPGA Driver
+
+module Aplication_4 (
+	input clk50MHz, // Main clock 50 MHz
+	input rst_n, // Reset button
+	output [6:0] Segments, // Cathode segments control outputs 
+	output dp // DP on Piswords PIAX301V2 FPGA board control output
+);	
+
+// signals for connection between modules
+wire clk2Hz;
+wire [3:0] HEX_data;	
+
+// we declare the components
+HEX_to_1_Digit_CC decoder
+(
+	.HEX_in		(HEX_data),
+	.dp_in		(HEX_data[0]),
+	.Segments 	(Segments),
+	.dp			(dp)
+);
+
+Clk_2Hz clk_2Hz
+(
+	.clk_in 		(clk50MHz),
+	.rst_n		(rst_n),
+	.clk_out 	(clk2Hz)
+);
+
+Counter16 count
+(
+	.clk 	   	(clk2Hz),
+	.rst_n		(rst_n),
+	.count 		(HEX_data)
+);
+	
+endmodule
